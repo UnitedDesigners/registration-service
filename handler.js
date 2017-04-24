35faf83c6bd05915be74d1bdec90a80a37391dc4
Zachary Riedlshah <zacharyrs@users.noreply.github.com>
2017-04-24 16:43:07 +1200

    Update handler.js

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ var notify = require('./notify');
 
 
 exports.handler = function(evt, cxt, cb) {
-    if (!validator.isEmpty(evt.name) || !validator.isEmail(evt.email)) {
+    if (!validator.isEmpty(evt.name) && validator.isEmail(evt.email)) {
         notify.notifySlack(evt).then(() => cb(null, 'Success'));
     }
     cb(new Error('Invalid Request'));
