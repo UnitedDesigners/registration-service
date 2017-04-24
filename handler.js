d93fca8865e5e172fc2650588e91660430afde2e
George Sumpster <gsumpster@gmail.com>
2017-04-23 20:15:52 -0700

    initial commit

---
+++ handler.js
@@ -0,0 +1,11 @@
+var validator = require('validator');
+
+var notify = require('./notify');
+
+
+exports.handler = function(evt, cxt, cb) {
+    if (!validator.isEmpty(evt.name) || !validator.isEmail(evt.email)) {
+        notify.notifySlack(evt).then(() => cb(null, 'Success'));
+    }
+    cb(new Error('Invalid Request'));
+};
---
+++ notify.js
@@ -0,0 +1,54 @@
+var slack = require('slack');
+
+module.exports = {
+    notifySlack: function(formData) {
+        return new Promise((resolve, reject) => {
+            slack.chat.postMessage({
+                token: process.env.SLACK_TOKEN,
+                channel: 'applications',
+                text: '',
+                attachments: constructMessage(formData)
+            }, (err, data) => {
+                if (err)
+                    reject(err);
+                if (data) {
+                    if (data.warning)
+                        slack.chat.postMessage({token: process.env.SLACK_TOKEN, channel: 'bot-debug', text: `An error has occurred\n ${data.warning}`});
+                    resolve(data);
+                }
+            });
+        });
+    }
+};
+
+function constructMessage(formData) {
+    return [
+        {
+            fallback: 'New Application Recieved!',
+            title: 'Application to United Designers',
+            fields: [
+                {
+                    title: 'Name',
+                    value: formData.name,
+                    short: true
+                }, {
+                    title: 'Email',
+                    value: formData.email,
+                    short: true
+                }, {
+                    title: 'Field',
+                    value: formData.field,
+                    short: true
+                }, {
+                    title: 'Portfolio',
+                    value: formData.portfolio,
+                    short: true
+                }, {
+                    title: 'Comments',
+                    value: formData.comments,
+                    short: true
+                }
+            ]
+        }
+    ];
+}
