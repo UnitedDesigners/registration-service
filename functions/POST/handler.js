2881c31b3e81ad4713fa6917b683c92eb96a1e6e
George Sumpster <gsumpster@gmail.com>
2017-05-06 19:47:10 -0700

    george pls

--- functions/POST/handler.js
+++ functions/POST/handler.js
@@ -1,20 +1,19 @@
-const invite = require('./slack').invite;
-const updateApplication = require('./dynamo').updateApplication;
+const slack = require('./slack');
+const dynamo = require('./dynamo');
 
 exports.handler = function(evt, cxt, cb) {
     if (evt) {
         let org_attachments = evt.original_message.attachments;
         org_attachments[0].actions = [];
         if (evt.actions[0].value === 'accept') {
-            invite(evt.callback_id).then(() => {updateApplication(evt.callback_id, evt.actions[0].value, evt.user.name);}).then(() => {
-                org_attachments[1] = {'fallback': `${evt.user.name} has accepted the application from ${evt.callback_id}`, text: `${evt.user.name} has accepted the application from ${evt.callback_id}`};
-                cb(null, {'response_type': 'in_channel','replace_original': true, attachments: org_attachments});
-            });
+            slack.invite(evt.callback_id);
+            dynamo.updateApplication(evt.callback_id, evt.actions[0].value, evt.user.name);
+            org_attachments[1] = {'fallback': `${evt.user.name} has accepted the application from ${evt.callback_id}`, text: `${evt.user.name} has accepted the application from ${evt.callback_id}`};
+            cb(null, {'response_type': 'in_channel','replace_original': true, attachments: org_attachments});
         } else {
-            updateApplication(evt.callback_id, evt.actions[0].value, evt.user.name).then(() => {
-                org_attachments[1] = {'fallback': `${evt.user.name} has denied the application from ${evt.callback_id}`, text: `${evt.user.name} has denied the application from ${evt.callback_id}`};
-                cb(null, {'response_type': 'in_channel','replace_original': true, attachments: org_attachments});
-            });
+            dynamo.updateApplication(evt.callback_id, evt.actions[0].value, evt.user.name);
+            org_attachments[1] = {'fallback': `${evt.user.name} has denied the application from ${evt.callback_id}`, text: `${evt.user.name} has denied the application from ${evt.callback_id}`};
+            cb(null, {'response_type': 'in_channel','replace_original': true, attachments: org_attachments});
         }
     } else {
         cb(new Error());
--- functions/POST/slack.js
+++ functions/POST/slack.js
@@ -1,7 +1,7 @@
 var tiny = require('tiny-json-http');
 var url = 'https://slack.com/api/users.admin.invite';
 
-module.export = {
+module.exports = {
     invite: function(email) {
         return new Promise((resolve, reject) => {
             tiny.post({url, data: { token: process.env.SLACK_TOKEN, email}}, function __got(err, result) {
